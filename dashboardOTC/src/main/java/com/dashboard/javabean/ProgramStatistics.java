c37ed31752eb6d6a48feb0dfdabdf8cf1f2e2971
735706 <735706@01HW463617.India.TCS.com>
2016-02-22 13:27:50 +0530

    OK

---
+++ dashboardOTC/src/main/java/com/dashboard/javabean/ProgramStatistics.java
@@ -0,0 +1,49 @@
+package com.dashboard.javabean;
+
+/**
+ * 
+ */
+
+/**
+ * @author Atrayee
+ *
+ */
+public class ProgramStatistics {
+	String Team;
+	String ProgramName;
+	String Weight;
+	String TargetTime;
+	String ActualTime;
+	public String getTeam() {
+		return Team;
+	}
+	public void setTeam(String team) {
+		Team = team;
+	}
+	public String getProgramName() {
+		return ProgramName;
+	}
+	public void setProgramName(String programName) {
+		ProgramName = programName;
+	}
+	public String getWeight() {
+		return Weight;
+	}
+	public void setWeight(String weight) {
+		Weight = weight;
+	}
+	public String getTargetTime() {
+		return TargetTime;
+	}
+	public void setTargetTime(String targetTime) {
+		TargetTime = targetTime;
+	}
+	public String getActualTime() {
+		return ActualTime;
+	}
+	public void setActualTime(String actualTime) {
+		ActualTime = actualTime;
+	}
+	
+	
+}
