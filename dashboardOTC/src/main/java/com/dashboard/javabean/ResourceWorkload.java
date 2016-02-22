e3cedf66c96a326c3b098a3d433bfe21c2722cf3
735706 <735706@01HW463617.India.TCS.com>
2016-02-22 19:31:17 +0530

    OK.

---
+++ dashboardOTC/src/main/java/com/dashboard/javabean/ResourceWorkload.java
@@ -0,0 +1,48 @@
+package com.dashboard.javabean;
+
+public class ResourceWorkload {
+	String Team;
+	String Analyst;
+	int Urgent;
+	int High;
+	int Medium;
+	int Low;
+	public String getTeam() {
+		return Team;
+	}
+	public void setTeam(String team) {
+		Team = team;
+	}
+	public String getAnalyst() {
+		return Analyst;
+	}
+	public void setAnalyst(String analyst) {
+		Analyst = analyst;
+	}
+	public int getUrgent() {
+		return Urgent;
+	}
+	public void setUrgent(int urgent) {
+		Urgent = urgent;
+	}
+	public int getHigh() {
+		return High;
+	}
+	public void setHigh(int high) {
+		High = high;
+	}
+	public int getMedium() {
+		return Medium;
+	}
+	public void setMedium(int medium) {
+		Medium = medium;
+	}
+	public int getLow() {
+		return Low;
+	}
+	public void setLow(int low) {
+		Low = low;
+	}
+	
+	
+}
