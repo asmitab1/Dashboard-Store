8d55f7d3ee8b76bc6e8da31e1abf203430cad108
735706 <735706@01HW463617.India.TCS.com>
2016-02-23 14:51:51 +0530

    OK

---
+++ dashboardOTC/src/main/java/com/dashboard/javabean/DefectResolution.java
@@ -0,0 +1,41 @@
+package com.dashboard.javabean;
+
+public class DefectResolution {
+	
+	private String team;
+	private String ticketNumber;
+	private String issueDescription;
+	private String resolution;
+	private String mitigatedRiskOpportunity;
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	public String getTicketNumber() {
+		return ticketNumber;
+	}
+	public void setTicketNumber(String ticketNumber) {
+		this.ticketNumber = ticketNumber;
+	}
+	public String getIssueDescription() {
+		return issueDescription;
+	}
+	public void setIssueDescription(String issueDescription) {
+		this.issueDescription = issueDescription;
+	}
+	public String getResolution() {
+		return resolution;
+	}
+	public void setResolution(String resolution) {
+		this.resolution = resolution;
+	}
+	public String getMitigatedRiskOpportunity() {
+		return mitigatedRiskOpportunity;
+	}
+	public void setMitigatedRiskOpportunity(String mitigatedRiskOpportunity) {
+		this.mitigatedRiskOpportunity = mitigatedRiskOpportunity;
+	}
+	
+}
