81571a82de4bb81aec9e411251c713a7f9057e26
735706 <735706@01HW463617.India.TCS.com>
2016-02-24 20:26:43 +0530

    OK

---
+++ dashboardOTC/src/main/java/com/dashboard/javabean/DefectAssignment.java
@@ -0,0 +1,35 @@
+package com.dashboard.javabean;
+
+public class DefectAssignment {
+	
+	private String project;
+	private String storyId;
+	private String storyDesc;
+	private String analyst;
+	public String getProject() {
+		return project;
+	}
+	public void setProject(String project) {
+		this.project = project;
+	}
+	public String getStoryId() {
+		return storyId;
+	}
+	public void setStoryId(String storyId) {
+		this.storyId = storyId;
+	}
+	public String getStoryDesc() {
+		return storyDesc;
+	}
+	public void setStoryDesc(String storyDesc) {
+		this.storyDesc = storyDesc;
+	}
+	public String getAnalyst() {
+		return analyst;
+	}
+	public void setAnalyst(String analyst) {
+		this.analyst = analyst;
+	}
+	
+	
+}
