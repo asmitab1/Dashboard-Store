b9b784ab6a45540fb46243a9764e08820737c894
asmitab1 <377447@01HW157370.India.TCS.com>
2016-03-02 22:01:06 +0530

    task

---
+++ dashboardOTC/src/main/java/com/dashboard/javabean/TaskAssignment.java
@@ -0,0 +1,72 @@
+package com.dashboard.javabean;
+
+public class TaskAssignment {
+	String team;
+	String developerName;
+	String taskName;
+	int totalPD;
+	int percentageCompl;
+	/**
+	 * @return the team
+	 */
+	public String getTeam() {
+		return team;
+	}
+	/**
+	 * @param team the team to set
+	 */
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	/**
+	 * @return the developerName
+	 */
+	public String getDeveloperName() {
+		return developerName;
+	}
+	/**
+	 * @param developerName the developerName to set
+	 */
+	public void setDeveloperName(String developerName) {
+		this.developerName = developerName;
+	}
+	/**
+	 * @return the taskName
+	 */
+	public String getTaskName() {
+		return taskName;
+	}
+	/**
+	 * @param taskName the taskName to set
+	 */
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+	/**
+	 * @return the totalPD
+	 */
+	public int getTotalPD() {
+		return totalPD;
+	}
+	/**
+	 * @param totalPD the totalPD to set
+	 */
+	public void setTotalPD(int totalPD) {
+		this.totalPD = totalPD;
+	}
+	/**
+	 * @return the percentageCompl
+	 */
+	public int getPercentageCompl() {
+		return percentageCompl;
+	}
+	/**
+	 * @param percentageCompl the percentageCompl to set
+	 */
+	public void setPercentageCompl(int percentageCompl) {
+		this.percentageCompl = percentageCompl;
+	}
+
+	
+	
+}
