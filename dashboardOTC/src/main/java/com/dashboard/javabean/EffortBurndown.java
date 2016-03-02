20d2596b946073d1f9719e3efc597a252861b844
Debarghya Chakraborty <debarghya88@gmail.com>
2016-03-02 12:26:32 +0530

    Changes for Burndown chart

---
+++ dashboardOTC/src/main/java/com/dashboard/javabean/EffortBurndown.java
@@ -0,0 +1,34 @@
+package com.dashboard.javabean;
+
+public class EffortBurndown {
+	private String week;
+	private int pdLeft;
+	private int idealpdLeft;
+	private int dev;
+	public String getWeek() {
+		return week;
+	}
+	public void setWeek(String week) {
+		this.week = week;
+	}
+	public int getPdLeft() {
+		return pdLeft;
+	}
+	public void setPdLeft(int pdLeft) {
+		this.pdLeft = pdLeft;
+	}
+	public int getIdealpdLeft() {
+		return idealpdLeft;
+	}
+	public void setIdealpdLeft(int idealpdLeft) {
+		this.idealpdLeft = idealpdLeft;
+	}
+	public int getDev() {
+		return dev;
+	}
+	public void setDev(int dev) {
+		this.dev = dev;
+	}
+	
+	
+}
