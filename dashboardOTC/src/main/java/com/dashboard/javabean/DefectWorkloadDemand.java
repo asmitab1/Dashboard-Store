bf59241199111cb2155e5e76c1afa912f838e5e3
Debarghya Chakraborty <debarghya88@gmail.com>
2016-03-03 11:55:47 +0530

    push

---
+++ dashboardOTC/src/main/java/com/dashboard/javabean/DefectWorkloadDemand.java
@@ -0,0 +1,46 @@
+package com.dashboard.javabean;
+
+public class DefectWorkloadDemand {
+	 private String team;
+	 private String analyst;
+	 private String veryHigh;
+	 private int high;
+	 private int medium;
+	 private int low;
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	public String getAnalyst() {
+		return analyst;
+	}
+	public void setAnalyst(String analyst) {
+		this.analyst = analyst;
+	}
+	public String getVeryHigh() {
+		return veryHigh;
+	}
+	public void setVeryHigh(String veryHigh) {
+		this.veryHigh = veryHigh;
+	}
+	public int getHigh() {
+		return high;
+	}
+	public void setHigh(int high) {
+		this.high = high;
+	}
+	public int getMedium() {
+		return medium;
+	}
+	public void setMedium(int medium) {
+		this.medium = medium;
+	}
+	public int getLow() {
+		return low;
+	}
+	public void setLow(int low) {
+		this.low = low;
+	}
+}
