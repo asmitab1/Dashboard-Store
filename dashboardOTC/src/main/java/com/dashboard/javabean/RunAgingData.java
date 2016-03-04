bd47c4ef1853de046d2b87d224d1930b9deb1edd
Debarghya Chakraborty <debarghya88@gmail.com>
2016-03-04 12:51:30 +0530

    run Aging data

---
+++ dashboardOTC/src/main/java/com/dashboard/javabean/RunAgingData.java
@@ -0,0 +1,48 @@
+package com.dashboard.javabean;
+
+public class RunAgingData {
+	private String team;
+	private int firstQuat;
+	private int secondQuat;
+	private int thirdQuat;
+	private int fourthQuat;
+	private int total;
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	public int getFirstQuat() {
+		return firstQuat;
+	}
+	public void setFirstQuat(int firstQuat) {
+		this.firstQuat = firstQuat;
+	}
+	public int getSecondQuat() {
+		return secondQuat;
+	}
+	public void setSecondQuat(int secondQuat) {
+		this.secondQuat = secondQuat;
+	}
+	public int getThirdQuat() {
+		return thirdQuat;
+	}
+	public void setThirdQuat(int thirdQuat) {
+		this.thirdQuat = thirdQuat;
+	}
+	public int getFourthQuat() {
+		return fourthQuat;
+	}
+	public void setFourthQuat(int fourthQuat) {
+		this.fourthQuat = fourthQuat;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
+	
+}
