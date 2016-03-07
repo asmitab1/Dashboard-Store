413a6d79468739ccee0cfb5a7381571f6a7646af
Debarghya Chakraborty <debarghya88@gmail.com>
2016-03-07 19:29:11 +0530

    Changes

--- dashboardOTC/src/main/webapp/js/dashboard.js
+++ dashboardOTC/src/main/webapp/js/dashboard.js
@@ -1472,7 +1472,7 @@ $(projectContainer).find(".chart-03").html(taskTable);
 	
 	
 	gantt.config.columns = [
-		{name:"text",       label:"Program Name (Weight)",  width:"*", tree:true }	
+		{name:"text",       label:"Program Name (% Completed)",  width:"*", tree:true }	
 	];
 	gantt.config.grid_width = 190;
 	gantt.config.min_column_width = 10;
